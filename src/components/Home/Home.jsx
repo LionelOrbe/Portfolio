5ff05fa8811a617dcd07764218aa19a3168fe314
LionelOrbe <lionel05@gmail.com>
2022-07-27 14:57:19 -0300

    Home css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import Footer from '../Footer/Footer'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <NavBar/>
+      Home
+        <Footer/>
+    </div>
   )
 }
