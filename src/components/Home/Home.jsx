572576709da31da8b5730e809140a31af786f948
LionelOrbe <lionel05@gmail.com>
2022-08-01 10:28:57 -0300

    Card components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import style from '../Home/Home.module.css'
+import Contact from '../Contact/Contact'
+import About from '../About/About'
+import Skills from '../Skills/Skills'
+import Work from '../Work/Work'
+
 
 export default function Home() {
   return (
-    <div>
+    <div className={style.container}>
         <NavBar/>
-      Home
+        <div className={style.homecontainer}>
+          <div>
+            <About/>
+          </div>
+          <div>
+            <Skills/>
+          </div>
+          <div>
+            <Work/>
+          </div>
+          <div>
+            <Contact/>
+          </div>
+        </div>
         <Footer/>
     </div>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,27 +3,28 @@ import { BsLinkedin ,BsGithub,BsFileEarmarkPerson} from "react-icons/bs";
 import style from '../NavBar/NavBar.module.css'
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import pdf from '../../assets/Lionel Orbe - Full Stack - EN.pdf'
 
 export default function NavBar() {
   return (
     <div className={style.container}>
       <Tippy content="LinkedIn">
         <div>
-        <a href="https://www.linkedin.com/in/lionel-orbe/" target="_blank">
+        <a href="https://www.linkedin.com/in/lionel-orbe/" target="_blank" rel="noreferrer">
           <BsLinkedin className={style.icon}/>
         </a>
         </div>
       </Tippy>
       <Tippy content="GitHub">
         <div>
-        <a href="https://github.com/LionelOrbe" target="_blank">
+        <a href="https://github.com/LionelOrbe" target="_blank" rel="noreferrer">
           <BsGithub className={style.icon}/>
         </a>
         </div>
       </Tippy>
       <Tippy content="Download my CV">
         <div>
-        <a href="https://www.linkedin.com/in/lionel-orbe/" target="_blank">
+        <a href={pdf} target="_blank" rel="noreferrer" download='Lionel Orbe - Full Stack - EN.pdf'>
           <BsFileEarmarkPerson className={style.icon}/>
         </a>
         </div>
