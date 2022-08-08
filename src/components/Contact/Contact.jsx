430b3dfe7a2035935ea7a9b2a3dfa7311f13f48c
LionelOrbe <lionel05@gmail.com>
2022-08-08 12:06:45 -0300

    Work ready

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,9 @@ import style from './Contact.module.css'
 
 export default function Contact() {
   return (
-    <div className={style.container}>Contact</div>
+    <div className={style.container}>
+      Get in Touch
+   
+    </div>
   )
 }
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import style from './Work.module.css'
-
+import concer from '../../assets/concertek.jpg'
+import count from '../../assets/countries.png'
 
 export default function Work() {
   return (
     <div className={style.container}>
-      <div>concertek</div>
-      <div>countries</div>
+      <div className={style.card}>
+        <div className={style.title}>ConcerTeck</div>
+        <a href="https://concer-teck-front-end.vercel.app/" target="_blank" rel="noreferrer">
+          <img className={style.img} src={concer} alt='concertek'/>
+        </a>
+        <div className={style.info}>E-commerce dedicated to event ticket sales</div>
+      </div>
+      <div className={style.card}>
+        <div className={style.title}>Countries App</div>
+        <a href="https://www.youtube.com/watch?v=rtmtSnT4Np4&t=6s" target="_blank" rel="noreferrer">
+         <img className={style.img} src={count} alt='concertek'/>
+        </a>
+        <div className={style.info}>Countries information App</div>
+      </div>
     </div>
   )
 }
