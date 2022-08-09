0c7e6a192b74eb60f4ae67e4b80b90b62690a93c
LionelOrbe <lionel05@gmail.com>
2022-08-09 11:47:52 -0300

    404 redirect

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import {Navigate} from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <Navigate to="/" />
+    </div>
   )
 }
