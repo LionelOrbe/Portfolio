c265dbea367f21b7cabd7c24f73774804303440a
LionelOrbe <lionel05@gmail.com>
2022-09-02 15:49:07 -0300

    Works Added

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import style from './Work.module.css'
 import concer from '../../assets/concertek.jpg'
 import count from '../../assets/Countries.png'
+import port from '../../assets/portfolio.jpg'
+import marvel from '../../assets/marvel.png'
 
 export default function Work() {
   return (
@@ -19,6 +21,20 @@ export default function Work() {
          <img className={style.img} src={count} alt='concertek'/>
         </a>
         <div className={style.info}>Countries information App</div>
+      </div>
+      <div className={style.card}>
+        <div className={style.title}>Portfolio</div>
+        <a href="https://eugeniabartolome.vercel.app/home" target="_blank" rel="noreferrer">
+         <img className={style.img} src={port} alt='portfolio'/>
+        </a>
+        <div className={style.info}>Translator portfolio App</div>
+      </div>
+      <div className={style.card}>
+        <div className={style.title}>Marvel App</div>
+        <a href="https://drive.google.com/file/d/1LC5x2Y0GWNaLDx5o6a9wl1fstXJRjhFF/view?usp=sharing" target="_blank" rel="noreferrer">
+         <img className={style.img} src={marvel} alt='marvel'/>
+        </a>
+        <div className={style.info}>React Native Mobile App</div>
       </div>
     </div>
   )
